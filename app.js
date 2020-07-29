f93ab2cdf123f11000e8088ae4fb79a6179a734b
pjflux2001 <pj.flux2001@gmail.com>
2020-07-29 20:20:49 +0530

    sendgrid init

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var express = require("express"),
 	bodyParser = require("body-parser"),
 	mongoose = require("mongoose"),
 	pug = require("pug"),
-	Esri = require("./models/esri.js")
+	sgMail = require('@sendgrid/mail'),
+	Esri = require("./models/esri.js");
 
 //MIDDLEWARE for Authentication
 
@@ -196,6 +197,31 @@ app.get("*", (req,res) => {
     res.render("404.ejs");
   });
 
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+app.post("/email",function(req,res){
+	var email =  req.body.email;
+	var name =  req.body.name;
+	var number = req.body.number;
+	var age = req.body.age;
+	var gender = req.body.gender;
+	var country = req.body.country;
+	var state = req.body.state;
+	var city = req.body.city;
+	var bg = req.body.bg;
+	console.log(email);
+	sgMail.setApiKey("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");
+	const msg = {
+	to: email,
+	from: 'pj.flux2001@gmail.com',
+	subject: 'Team BITSians : Success',
+	text: 'Dear '+name+', Your form has been successfully pushed into our database. It will expire in next 72 hours, hence take the required steps to comply with the same. The information that was pushed into this form : Age : <br>Gender : <br>Country : <br>State : <br>City : <br>Blood Group :'+ bg +' We will contact you @'+number+' in case of any emergency. Best Regards, Team BITSians ',
+	html: 'Dear '+name+',<br><br>Your form has been successfully pushed into our database. It will expire in next <strong>72 hours</strong>, hence take the required steps to comply with the same.<br><br>The information that was pushed into this form :<br><p>Age : '+age+'<br>Gender : '+gender+'<br>Country : '+country+'<br>State : '+state+'<br>City : '+city+'<br>Blood Group : '+ bg +'<br></p>We will contact you @'+number+' in case of any emergency.<br><br> Best Regards,<br> Team BITSians',
+	};
+	sgMail.send(msg);
+	res.render("common_pool");
+})
 
 //========================//
 //LISTENER PROCESS
